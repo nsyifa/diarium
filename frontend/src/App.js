11b8dee5f72ba8775ec746494b09df8a9f0e2d8e
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2023-12-20 22:16:15 +0900

    create main app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useUserContext } from "./context/UserContext";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Analytics from "./pages/Analytics";
+import Entries from "./pages/Entries";
+import Settings from "./pages/Settings";
+import ReadEntry from "./pages/ReadEntry";
 
 function App() {
-  return <div className="App text-yellow-500">sup</div>;
+  const { user } = useUserContext();
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/analytics" element={<Analytics />} />
+      <Route path="/entries" element={<Entries />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/read" element={<ReadEntry />} />
+    </Routes>
+  );
 }
 
 export default App;
