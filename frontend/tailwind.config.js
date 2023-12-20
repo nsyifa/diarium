27ade030cdf29524131ed972ad1fd8bcb8d2b2e3
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2023-12-20 22:13:45 +0900

    added custom classes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+      colors: {
+        themeblack: "#313638",
+        lightcreme: "#EFE8E1",
+      },
+      gridTemplateRows: {
+        11: "repeat(11, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
